5b645d7187f7433b8dea059c3c1ef75acae02a09
Fouad Macari <fidriss96@gmail.com>
2026-02-05 14:16:41 +0100

    feat: Add .gitignore and .env.example files; implement ping route in API

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -3,6 +3,11 @@ const express = require('express');
 const router = express.Router();
 const userRoutes = require('./userRoutes');
 
+// Route de test pour vérifier le fonctionnement de l'API
+router.get('/ping', (req, res) => {
+	res.json({ message: "L'API fonctionne bien" });
+});
+
 router.use('/users', userRoutes);
 
 module.exports = router;
